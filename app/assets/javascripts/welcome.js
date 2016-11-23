1c2a5ef4b5ee89683298ca2e83095bfc8d83756f
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-11-22 16:45:06 -0800

    Created animation for the cut-off background. Need to animate the frame. So far it appears with hidden/block display manipulation

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,4 +3,5 @@ window.setTimeout(addFrame, 1500);
 function addFrame() {
   console.log("add frame function");
   $('.show-border').css('display', 'block');
+  $('.cut-out-background').css('transform', 'rotate(-15deg) translateY(0)');
 }
